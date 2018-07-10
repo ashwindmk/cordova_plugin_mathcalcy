b060a57b5d279a1e416f664a70e08021c9444bb4
Ashwin <ashwin.dinesh01@gmail.com>
2018-07-10 19:52:52 +0530

    Fixed success is not defined error

--- www/MathCalcy.js
+++ www/MathCalcy.js
@@ -4,6 +4,6 @@ module.exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'MathCalcy', 'coolMethod', [arg0]);
 };
 
-module.exports.add = function() {
+module.exports.add = function(arg0, success, error) {
     exec(success, error, 'MathCalcy', 'add', [arg0]);
 };
